f1a0c69b2465bdbf4fbc802e47852e2456cd60fa
Vidrequin Olivier <vidrequin.olivier@gmail.com>
2024-10-05 17:48:45 +0200

    Début du développement du thême sombre

--- js/index.js
+++ js/index.js
@@ -1,3 +1,36 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const toggleButton = document.getElementById('themeToggleButton');
+  const currentTheme = localStorage.getItem('theme') || 'light';
+	const darkThemeItemsList = 	[ 
+		document.body, 
+		document.querySelector(".linearGradient"), 
+		document.querySelector("h1"), 
+		document.querySelector(".contactMe")
+	];
+
+  if (currentTheme === 'dark') {
+		darkThemeItemsList.forEach((element) => {
+			if (element) {
+				element.classList.add('darkTheme');
+			}
+		});
+    toggleButton.textContent = 'Switch to Light Mode';
+  }
+
+  toggleButton.addEventListener('click', () => {
+		darkThemeItemsList.forEach((element) => {
+			if (element) {
+				element.classList.toggle('darkTheme');
+			}
+		});
+		
+    const newTheme = document.body.classList.contains('darkTheme') ? 'dark' : 'light';
+
+    toggleButton.textContent = newTheme === 'dark' ? 'Switch to Light Mode' : 'Switch to Dark Mode';
+    localStorage.setItem('theme', newTheme);
+  });
+});
+
 let currentSlide = 0;
 const slides = document.querySelectorAll('.slide');
 
@@ -36,4 +69,3 @@ function changeSlide(direction) {
 };
 
 showSlide(currentSlide);
-
